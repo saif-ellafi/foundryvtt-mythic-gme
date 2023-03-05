3c63ebdd9fd116f17abe857e1662d56909591d6e
vulpzor <anon@example.com>
2023-03-04 19:48:16 -0500

    Add support for Mythic 2e Fate Chart and Event Focus table

---
+++ src/app/panel-mythic-gme-2e.js
@@ -0,0 +1,38 @@
+import MGMEPanel from "./panel-base";
+import '../style/panel-mythic.css'
+
+export default class MGMEBluePanel extends MGMEPanel {
+
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      id: "m2e_panel_window",
+      title: 'Mythic GM Emulator 2e',
+      template: "./modules/mythic-gme-tools/template/panel-mythic-gme-2e.hbs"
+    });
+  }
+
+  getData() {
+    return mergeObject(super.getData(), {
+      rankValue: game.settings.get('mythic-gme-tools', 'currentChaos')
+    });
+  }
+
+  activateListeners(html) {
+    const api = game.modules.get('mythic-gme-tools').api;
+
+    html.find("#mgme_panel_fq").click(api.mgmeFateChart);
+    html.find("#mgme_panel_sa").click(api.mgmeSceneAlteration);
+
+    html.find("#mgme_panel_re").click(api.mgmeRandomEvent);
+    html.find("#mgme_panel_fo").click(api.mgmeFocusCheck);
+
+    html.find("#mgme_panel_npc").click(api.mgmeRenderNPCsList);
+    html.find("#mgme_panel_npc_roll").click(api.mgmeRollNPCsList);
+    html.find("#mgme_panel_thread").click(api.mgmeRenderThreadsList);
+    html.find("#mgme_panel_thread_roll").click(api.mgmeRollThreadsList);
+
+    html.find("#mgme_panel_ci").click(api.mgmeIncreaseChaos);
+    html.find("#mgme_panel_cd").click(api.mgmeDecreaseChaos);
+  }
+
+}
